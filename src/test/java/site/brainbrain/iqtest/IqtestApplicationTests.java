e5ed23a3736c4d68d1c11e125f2b4766d1fa0add
reddevilmidzy <midzy0228@gmail.com>
2025-05-01 23:44:13 +0900

    fix: test 실행 시 test 프로파일 적용

--- src/test/java/site/brainbrain/iqtest/IqtestApplicationTests.java
+++ src/test/java/site/brainbrain/iqtest/IqtestApplicationTests.java
@@ -2,12 +2,14 @@ package site.brainbrain.iqtest;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class IqtestApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
