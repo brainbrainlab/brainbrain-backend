5f5e553768f0ddccb3512651459dba55ef294433
lemone <lejinhy@gmail.com>
2025-05-06 14:41:56 +0900

    fix: id 필드에 GeneratedValue 어노테이션 추가

--- src/main/java/site/brainbrain/iqtest/domain/Payment.java
+++ src/main/java/site/brainbrain/iqtest/domain/Payment.java
@@ -2,6 +2,8 @@ package site.brainbrain.iqtest.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import java.time.OffsetDateTime;
 import lombok.Getter;
@@ -11,6 +13,7 @@ import lombok.Getter;
 public class Payment {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     @Column(name = "order_id", unique = true)
