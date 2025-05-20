f719f3c685529f7a6cc52bdef5333d9f5557a6ff
yechop <channetattoo@gmail.com>
2025-05-20 20:15:41 +0900

    chore: PaymentService 제네릭 타입에 대한 설명 주석

--- src/main/java/site/brainbrain/iqtest/service/payment/PaymentService.java
+++ src/main/java/site/brainbrain/iqtest/service/payment/PaymentService.java
@@ -1,5 +1,8 @@
 package site.brainbrain.iqtest.service.payment;
 
+/**
+ * @param <T> PG사별 결제 요청 DTO 타입 (토스: TossApiConfirmRequest, 나이스: NicePaymentCallbackRequest)
+ */
 public interface PaymentService<T> {
 
     void pay(final T request);
