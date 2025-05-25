cb5da94b2cbb722f662f34f72488c98c5db9f0d2
yechop <channetattoo@gmail.com>
2025-05-25 21:35:35 +0900

    chore: 나이스 요청 dto 디버깅 시도

--- src/main/java/site/brainbrain/iqtest/controller/PaymentController.java
+++ src/main/java/site/brainbrain/iqtest/controller/PaymentController.java
@@ -1,8 +1,11 @@
 package site.brainbrain.iqtest.controller;
 
+import java.util.Map;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
@@ -17,9 +20,15 @@ public class PaymentController {
 
     private final NicePaymentService paymentService;
 
+//    @PostMapping("/payments/confirm")
+//    public ResponseEntity<PaymentConfirmResponse> confirmPayment(@RequestBody final NicePaymentCallbackRequest request) {
+//        paymentService.pay(request);
+//        return ResponseEntity.ok(new PaymentConfirmResponse(request.orderId()));
+//    }
+
     @PostMapping("/payments/confirm")
-    public ResponseEntity<PaymentConfirmResponse> confirmPayment(@RequestBody final NicePaymentCallbackRequest request) {
-        paymentService.pay(request);
-        return ResponseEntity.ok(new PaymentConfirmResponse(request.orderId()));
+    public ResponseEntity<?> confirm(@RequestParam Map<String, String> params) {
+        params.forEach((k, v) -> System.out.println(k + " = " + v));
+        return ResponseEntity.ok().build();
     }
 }
