6f2473638d4db46ba68b61d5a1251dc650487585
yechop <channetattoo@gmail.com>
2025-05-26 18:02:32 +0900

    feat: orderId로 결제내역 db 조회 추가

--- src/main/java/site/brainbrain/iqtest/repository/NicePaymentRepository.java
+++ src/main/java/site/brainbrain/iqtest/repository/NicePaymentRepository.java
@@ -14,6 +14,6 @@ public interface NicePaymentRepository extends JpaRepository<NicePayment, Long> 
     Optional<NicePayment> findByOrderId(final String orderId);
 
     default NicePayment fetchByOrderId(final String orderId) {
-        return findByOrderId(orderId).orElseThrow(() -> new PaymentException("주문번호와 일치하는 결제 내역이 없습니다."));
+        return findByOrderId(orderId).orElseThrow(() -> new PaymentException("주문번호와 일치하는 나이스 결제 내역이 없습니다."));
     }
 }
--- src/main/java/site/brainbrain/iqtest/repository/TossPaymentRepository.java
+++ src/main/java/site/brainbrain/iqtest/repository/TossPaymentRepository.java
@@ -1,10 +1,19 @@
 package site.brainbrain.iqtest.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import site.brainbrain.iqtest.domain.payment.TossPayment;
+import site.brainbrain.iqtest.exception.PaymentException;
 
 @Repository
 public interface TossPaymentRepository extends JpaRepository<TossPayment, Long> {
+
+    Optional<TossPayment> findByOrderId(final String orderId);
+
+    default TossPayment fetchByOrderId(final String orderId) {
+        return findByOrderId(orderId).orElseThrow(() -> new PaymentException("주문번호와 일치하는 토스 결제 내역이 없습니다."));
+    }
 }
