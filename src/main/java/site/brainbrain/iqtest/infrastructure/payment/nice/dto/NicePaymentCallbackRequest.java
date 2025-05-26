97c2d73ce96458bd9130bb5edb5f6a8a2144b74a
yechop <channetattoo@gmail.com>
2025-05-26 18:01:57 +0900

    refactor: 결제 승인 요청 dto Map 을 파라미터로 받아오도록 변경

--- src/main/java/site/brainbrain/iqtest/infrastructure/payment/nice/dto/NicePaymentCallbackRequest.java
+++ src/main/java/site/brainbrain/iqtest/infrastructure/payment/nice/dto/NicePaymentCallbackRequest.java
@@ -1,5 +1,7 @@
 package site.brainbrain.iqtest.infrastructure.payment.nice.dto;
 
+import java.util.Map;
+
 public record NicePaymentCallbackRequest(String authResultCode,
                                          String authResultMsg,
                                          String tid,
@@ -9,4 +11,18 @@ public record NicePaymentCallbackRequest(String authResultCode,
                                          String mallReserved,
                                          String authToken,
                                          String signature) {
+
+    public static NicePaymentCallbackRequest from(final Map<String, String> params) {
+        return new NicePaymentCallbackRequest(
+                params.get("authResultCode"),
+                params.get("authResultMsg"),
+                params.get("tid"),
+                params.get("clientId"),
+                params.get("orderId"),
+                Integer.parseInt(params.get("amount")),
+                params.get("mallReserved"),
+                params.get("authToken"),
+                params.get("signature")
+        );
+    }
 }
