a042cb2f32d3836b636232d1c26be162ec2b3412
yechop <channetattoo@gmail.com>
2025-05-26 22:15:29 +0900

    feat: 쿠폰 사용 동시성 문제 방지

--- src/main/java/site/brainbrain/iqtest/controller/dto/CreateResultRequest.java
+++ src/main/java/site/brainbrain/iqtest/controller/dto/CreateResultRequest.java
@@ -2,7 +2,8 @@ package site.brainbrain.iqtest.controller.dto;
 
 import java.util.List;
 
-public record CreateResultRequest(String orderId,
+public record CreateResultRequest(String couponCode,
+                                  String orderId,
                                   UserInfoRequest userInfoRequest,
                                   List<Integer> answers) {
 }
--- src/main/java/site/brainbrain/iqtest/repository/CouponRepository.java
+++ src/main/java/site/brainbrain/iqtest/repository/CouponRepository.java
@@ -1,8 +1,12 @@
 package site.brainbrain.iqtest.repository;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import site.brainbrain.iqtest.domain.Coupon;
 import site.brainbrain.iqtest.exception.CouponException;
@@ -14,4 +18,10 @@ public interface CouponRepository extends JpaRepository<Coupon, Long> {
     default Coupon fetchByCode(final String couponCode) {
         return findByCode(couponCode).orElseThrow(() -> new CouponException("쿠폰을 찾을 수 없습니다."));
     }
+
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE Coupon "
+            + "SET isAvailable = false, usedAt = :usedAt "
+            + "WHERE code = :code AND isAvailable = true")
+    int markAsUsed(@Param("code") final String code, @Param("usedAt") final LocalDateTime usedAt);
 }
