0994652dc9e69712352080c5c29101a5c5998d11
yechop <channetattoo@gmail.com>
2025-06-02 20:35:10 +0900

    refactor: 검증 로직 nullsafe하게 변경

--- src/main/java/site/brainbrain/iqtest/infrastructure/payment/nice/util/NicePaymentValidator.java
+++ src/main/java/site/brainbrain/iqtest/infrastructure/payment/nice/util/NicePaymentValidator.java
@@ -1,5 +1,7 @@
 package site.brainbrain.iqtest.infrastructure.payment.nice.util;
 
+import java.util.Objects;
+
 import site.brainbrain.iqtest.exception.PaymentException;
 
 public class NicePaymentValidator {
@@ -7,13 +9,13 @@ public class NicePaymentValidator {
     private static final String SUCCESS_RESULT_CODE = "0000";
 
     public static void validateNiceResultCode(final String resultCode) {
-        if (!resultCode.equals(SUCCESS_RESULT_CODE)) {
+        if (!SUCCESS_RESULT_CODE.equals(resultCode)) {
             throw new PaymentException("결제 인증에 실패했습니다. result code : " + resultCode);
         }
     }
 
     public static void validateSignature(final String signature, final String expectedSignature) {
-        if (!signature.equals(expectedSignature)) {
+        if (!Objects.equals(signature, expectedSignature)) {
             throw new PaymentException("위변조 검증 데이터가 일치하지 않습니다.");
         }
     }
