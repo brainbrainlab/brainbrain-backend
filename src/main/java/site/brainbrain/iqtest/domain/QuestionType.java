43fb4c62d11102866586ffcd573f7b6e2a41e601
reddevilmidzy <midzy0228@gmail.com>
2025-06-02 19:20:01 +0900

    refactor: 불필요한 enum 필드 제거

--- src/main/java/site/brainbrain/iqtest/domain/QuestionType.java
+++ src/main/java/site/brainbrain/iqtest/domain/QuestionType.java
@@ -7,12 +7,11 @@ import lombok.RequiredArgsConstructor;
 @Getter
 public enum QuestionType {
 
-    VISUAL_PATTERN_RECOGNITION(0, "시각적 패턴인식"),
-    LOGICAL_REASONING_ABILITY(1, "논리적 추론 능력"),
-    SPATIAL_PERCEPTION(2, "공간 지각 능력"),
+    VISUAL_PATTERN_RECOGNITION("시각적 패턴인식"),
+    LOGICAL_REASONING_ABILITY("논리적 추론 능력"),
+    SPATIAL_PERCEPTION("공간 지각 능력"),
     ;
 
-    private final int typeValue;
     private final String name;
 
 }
