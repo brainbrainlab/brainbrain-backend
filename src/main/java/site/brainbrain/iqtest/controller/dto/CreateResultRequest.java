d1b5cc224d2a5491a37996aebf7fd08d48a4e3bf
yechop <channetattoo@gmail.com>
2025-06-13 18:31:29 +0900

    chore: dto 쿠폰코드 필드 제거

--- src/main/java/site/brainbrain/iqtest/controller/dto/CreateResultRequest.java
+++ src/main/java/site/brainbrain/iqtest/controller/dto/CreateResultRequest.java
@@ -2,8 +2,7 @@ package site.brainbrain.iqtest.controller.dto;
 
 import java.util.List;
 
-public record CreateResultRequest(String couponCode,
-                                  String orderId,
+public record CreateResultRequest(String orderId,
                                   UserInfoRequest userInfoRequest,
                                   List<Integer> answers) {
 }
