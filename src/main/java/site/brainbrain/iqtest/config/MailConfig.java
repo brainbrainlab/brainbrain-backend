362a6a3e5d9b746a503447eb7cb31892c4aea7a4
yechop <channetattoo@gmail.com>
2025-06-26 16:24:15 +0900

    chore: 메일 타임아웃설정, 디버깅 추가

--- src/main/java/site/brainbrain/iqtest/config/MailConfig.java
+++ src/main/java/site/brainbrain/iqtest/config/MailConfig.java
@@ -14,8 +14,11 @@ public class MailConfig {
     private static final String SMTP_HOST = "smtp.gmail.com";
     private static final int SMTP_PORT = 587;
     private static final String MAIL_SMTP_AUTH = "mail.smtp.auth";
-    private static final String MAIL_DEBUG = "mail.smtp.debug";
+    private static final String MAIL_SMTP_DEBUG = "mail.smtp.debug";
     private static final String MAIL_SMTP_STARTTLS_ENABLE = "mail.smtp.starttls.enable";
+    public static final String MAIL_SMTP_CONNECTION_TIMEOUT = "mail.smtp.connectiontimeout";
+    public static final String MAIL_SMTP_TIMEOUT = "mail.smtp.timeout";
+    public static final String MAIL_SMTP_WRITE_TIMEOUT = "mail.smtp.writetimeout";
 
     @Value("${spring.mail.username}")
     private String username;
@@ -35,8 +38,10 @@ public class MailConfig {
         final Properties properties = javaMailSender.getJavaMailProperties();
         properties.put(MAIL_SMTP_AUTH, true);
         properties.put(MAIL_SMTP_STARTTLS_ENABLE, true);
-        properties.put(MAIL_DEBUG, false);
-        // Todo: 타임아웃 설정
+        properties.put(MAIL_SMTP_DEBUG, true);
+        properties.put(MAIL_SMTP_CONNECTION_TIMEOUT, "10000");
+        properties.put(MAIL_SMTP_TIMEOUT, "10000");
+        properties.put(MAIL_SMTP_WRITE_TIMEOUT, "10000");
 
         javaMailSender.setJavaMailProperties(properties);
         return javaMailSender;
